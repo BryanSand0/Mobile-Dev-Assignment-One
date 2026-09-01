b0e5df7bb6351bd2c163882b327c896c48fe874d
Luke <lucnelnordelus@live.com>
2026-08-31 21:59:18 -0400

    part3 ProfileActivity name display

--- ProjectApp3/app/src/main/java/edu/charlotte/projectapp3/CreateUserActivity.java
+++ ProjectApp3/app/src/main/java/edu/charlotte/projectapp3/CreateUserActivity.java
@@ -22,23 +22,13 @@ public class CreateUserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_user);
-
         ViewCompat.setOnApplyWindowInsetsListener(
                 findViewById(R.id.main),
                 (v, insets) -> {
-                    Insets systemBars =
-                            insets.getInsets(WindowInsetsCompat.Type.systemBars());
-
-                    v.setPadding(
-                            systemBars.left,
-                            systemBars.top,
-                            systemBars.right,
-                            systemBars.bottom
-                    );
-
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
                     return insets;
                 });
 
--- ProjectApp3/app/src/main/java/edu/charlotte/projectapp3/ProfileActivity.java
+++ ProjectApp3/app/src/main/java/edu/charlotte/projectapp3/ProfileActivity.java
@@ -1,6 +1,8 @@
 package edu.charlotte.projectapp3;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView textViewName;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,20 @@ public class ProfileActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        setTitle("Profile");
+
+        textViewName = findViewById(R.id.textViewNameValue);
+        Button buttonUpdate = findViewById(R.id.buttonUpdate);
+
+        if (getIntent() != null && getIntent().hasExtra(CreateUserActivity.KEY_NAME)) {
+            name = getIntent().getStringExtra(CreateUserActivity.KEY_NAME);
+            textViewName.setText(name);
+        }
+
+        buttonUpdate.setOnClickListener(v -> {
+            // Part 4: Logic to launch EditUserActivity will go here
         });
     }
 }
